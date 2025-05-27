2e7e4f6e7260f17cf8a176c3c152ce8476b74298
timocheu <ccs.timotheoche@gmail.com>
2025-05-27 21:54:54 +0800

    Make the AST structures

---
+++ ast/ast.go
@@ -0,0 +1,56 @@
+package ast
+
+import "github.com/timocheu/kalayo/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	// takes an node interface
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	// takes an node interface
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	// return the first literal in the "Statements"
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Start of nodes types
+
+// Let holds the Let token itself, identifier, value
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
