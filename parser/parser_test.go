18af2e411a69c9f9ac1fc5f10a440355dbfdd617
timocheu <ccs.timotheoche@gmail.com>
2025-05-28 11:04:08 +0800

    Make parser, parse only var and identifiers

---
+++ parser/parser_test.go
@@ -0,0 +1,71 @@
+package parser_test
+
+import (
+	"testing"
+
+	"github.com/timocheu/kalayo/ast"
+	"github.com/timocheu/kalayo/lexer"
+	"github.com/timocheu/kalayo/parser"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+	var a = 10;
+	var b = 123;
+	var c = 44223;
+	`
+
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParserProgram() returned nil or failed")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program Statements does not contain 3 Statements. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"a"},
+		{"b"},
+		{"c"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "var" {
+		t.Errorf("s.TokenLiteral not 'var'. got=%q", s.TokenLiteral())
+	}
+
+	// Assert that s is an letStatement which is under the Statement interface
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s",
+			name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s",
+			name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
